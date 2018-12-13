3be32329a4a3a1d499e090e96727a1664ffbbca9
Jake <jayyf9@gmail.com>
2018-12-13 21:04:53 +0100

    Put all json data in one file and setup components to use that data.

--- Portfolio/Jake French/src/components/blog/blog.vue
+++ Portfolio/Jake French/src/components/blog/blog.vue
@@ -1,19 +1,11 @@
 <template src="./blog.html"></template>
 <script>
+  import blogData from "./blog-data.json";
+
   export default {
     data(){
       return {
-        posts: [
-          {
-            id: 0,
-            url: "cms",
-            img: "./src/images/blog-posts/cms/cms.jpg",
-            title: 'Should a developer be using a CMS?',
-            header: 'First of all, what is a CMS?',
-            content: 'Content management system’s are software application that runs on the web-server and allows a user to easily manage the content of their website. This uses an interface and does not require any programming knowledge to do.',
-            date: '11/11/11'
-          }
-        ]
+        posts: blogData
       } 
     }
   }
--- Portfolio/Jake French/src/components/portfolio/portfolio.vue
+++ Portfolio/Jake French/src/components/portfolio/portfolio.vue
@@ -1,34 +1,13 @@
 <template src="./portfolio.html"></template>
 <script>
+  import portfolioData from "./portfolio-data.json";
+
   export default {
     data(){
       return {
-        websites: [
-          {
-            id: 0,
-            img: "./src/images/portfolio-items/multione-web/multione-web-thumbnail.png",
-            client: "./src/images/clients/philips.jpg",
-            title: 'MultiOne Web',
-            content: 'This is a website for a local Cafe trying to get there business online for the first time and wanted a site that simply advertises there services in a classy and modern way.'
-          },
-          {
-            id: 1,
-            img: "./src/images/portfolio-items/altran-academy/altran-academy-thumbnail.png",
-            client: "./src/images/clients/altran.png",
-            title: 'Altran Academy',
-            content: 'A company training website that allows users to search filter and enroll on courses easily.'
-          },
-          {
-            id: 2,
-            img: "./src/images/portfolio-items/factory-of-the-future/fotf-thumbnail.png",
-            client: "./src/images/clients/brainport.jpg",
-            title: 'Factory of the future',
-            content: 'This is a website that allows users to look through other recipies uploaded by users and rate/give feedback allow for a communtiy of food lovers to come together.'
-          }
-        ]
+        websites: portfolioData
+      }
     }
-
-  }
   }
 </script>
 <style>
